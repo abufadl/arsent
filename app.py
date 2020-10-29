99e919991ed747aa427affda819527a468ab9640
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-29 11:58:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import time
 def main():
   st.title('Arabic Sentiment Analysis') # title
   st.subheader("Abed Khooli - @akhooli")
-
+  # Render the readme as markdown using st.markdown.
+  readme_text = st.markdown(get_file_content_as_string("README.md"))
   with st.spinner('Loading something ...'):
     time.sleep(2)
     
@@ -16,8 +17,21 @@ def main():
   
   
 def run_the_app():
-  slot1.text('run executed')
+  #slot1.text('run executed')
   return 'success'
+
+
+# Download a single file and make its content available as a string.
+@st.cache(show_spinner=False)
+def get_file_content_as_string(path):
+    url = 'https://raw.githubusercontent.com/abufadl/asa/' + path
+    response = urllib.request.urlopen(url)
+    return response.read().decode("utf-8")
+
+
+
+
+
 # main statup
 if __name__ == "__main__":
     main()
