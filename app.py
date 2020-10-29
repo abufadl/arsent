80ec2c6a8d32ea3902fa4f98994636af030b1465
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-29 12:30:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from fastai.text import *
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -9,15 +10,15 @@ import urllib
 
 
 def main():
-  st.title('Arabic Sentiment Analysis') # title
-  st.subheader("Abed Khooli - @akhooli")
-  # Render the readme as markdown using st.markdown.
-  readme_text = st.markdown(get_file_content_as_string("README.md"))
-  with st.spinner('Loading something ...'):
-    time.sleep(2)
+    st.title('Arabic Sentiment Analysis') # title
+    st.subheader("Abed Khooli - @akhooli")
+    # Render the readme as markdown using st.markdown.
+    readme_text = st.markdown(get_file_content_as_string("README.md"))
+    #with st.spinner('Loading something ...'):
+    #       time.sleep(2)
  
 
-    # Once we have the dependencies, add a selector for the app mode on the sidebar.
+  # Once we have the dependencies, add a selector for the app mode on the sidebar.
     st.sidebar.title("Main Menu")
     app_mode = st.sidebar.selectbox("Select an option",
         ["Show background info", "Run the app", "Show the source code"])
@@ -29,21 +30,21 @@ def main():
     elif app_mode == "Run the app":
         readme_text.empty()
         run_the_app()
-  # run the app 
-  #run_the_app()
+    # run the app 
+    #run_the_app()
   
   
 def run_the_app():
-  st.text("app ran successfully.")
-  return 'success'
+    st.text("app ran successfully.")
+    return 'success'
 
 
 # Download a single file and make its content available as a string. https://raw.githubusercontent.com/abufadl/asa/master/
 @st.cache(show_spinner=False)
 def get_file_content_as_string(path):
-  url = 'https://raw.githubusercontent.com/abufadl/asa/master/' + path
-  response = urllib.request.urlopen(url)
-  return response.read().decode("utf-8")
+    url = 'https://raw.githubusercontent.com/abufadl/asa/master/' + path
+    response = urllib.request.urlopen(url)
+    return response.read().decode("utf-8")
 
 
 
