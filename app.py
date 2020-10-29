44fa27caae740dfaf2eaa3de412b74aa79e6c253
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-29 10:58:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import time
 
 # title
 st.title('Arabic Sentiment Analysis')
+st.subheader("Abed Khooli - @akhooli")
+
+with st.spinner('Loading something ...'):
+  time.sleep(2)
+  
+#
