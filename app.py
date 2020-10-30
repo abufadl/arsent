ec74eb90cb75da1fca600ec37122825baa512957
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-30 21:14:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import torch
 
 
 def main():
-st.set_page_config(  # Alternate names: setup_page, page, layout
+	st.set_page_config(  # Alternate names: setup_page, page, layout
 	layout="centered",  # Can be "centered" or "wide". In the future also "dashboard", etc.
 	initial_sidebar_state="expanded",  # Can be "auto", "expanded", "collapsed"
 	page_title=None,  # String or None. Strings get appended with "• Streamlit". 
